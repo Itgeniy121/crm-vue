d836b27367f0272eecd02ccba2f012104272041c
Aleksey <120134806+Itgeniy121@users.noreply.github.com>
2023-06-07 01:08:48 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-})
-
-module.exports = {
+  transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/crm-vue/'
     : ''
-}
+});
+
+
+
